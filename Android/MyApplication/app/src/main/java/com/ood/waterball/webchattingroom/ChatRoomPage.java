a9550f2f08f80a38ad272975421de623483854a7
WaterBall <johnny850807@gmail.com>
2017-02-16 15:48:08 +0800

    聊天室首頁 回到 大廳

--- Android/MyApplication/app/src/main/java/com/ood/waterball/webchattingroom/ChatRoomPage.java
+++ Android/MyApplication/app/src/main/java/com/ood/waterball/webchattingroom/ChatRoomPage.java
@@ -5,12 +5,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class ChatRoomPage extends AppCompatActivity {
     private WebView chatWebView;  //聊天室web前端
-    private String chatroomURL = "http://waterball.lionfree.net/waterball.lionfree.net/johnny850807/ChatroomList.html?";
+    private String chatroomURL = "http://waterball.lionfree.net/waterball.lionfree.net/johnny850807/ChatRoom.html?";
 
     private static MediaPlayer mediaPlayer;  //背景音樂
 
@@ -25,6 +26,8 @@ public class ChatRoomPage extends AppCompatActivity {
 
     private void webviewSetting(){
         chatWebView = new WebView(this);
+        WebSettings webSettings = chatWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
         /***讓網址request在android內完成而非開啟新的瀏覽器***/
         chatWebView.setWebViewClient(new WebViewClient() {
