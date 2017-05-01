84420dcfe86d9fd410f0de708917bd1297726318
WaterBall <johnny850807@gmail.com>
2017-05-02 01:03:20 +0800

    首頁 layout

--- Android/TeamPathy/app/src/main/java/com/ood/waterball/teampathy/MainActivity.java
+++ Android/TeamPathy/app/src/main/java/com/ood/waterball/teampathy/MainActivity.java
@@ -2,6 +2,7 @@ package com.ood.waterball.teampathy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void forgotOnClick(View view) {
+
+    }
+
+    public void registerOnClick(View view) {
+
+    }
+
+    public void logInOnClickMain(View view) {
+
     }
 }
