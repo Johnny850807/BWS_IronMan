6d12a00ec0ec8ce44758bec403f125c854b15e59
WaterBall <johnny850807@gmail.com>
2017-05-02 14:06:16 +0800

    test controller completed

--- Android/TeamPathy/app/src/main/java/com/ood/waterball/teampathy/Controllers/MemberSystem/MemberController.java
+++ Android/TeamPathy/app/src/main/java/com/ood/waterball/teampathy/Controllers/MemberSystem/MemberController.java
@@ -1,9 +1,14 @@
 package com.ood.waterball.teampathy.Controllers.MemberSystem;
 
 
+import com.ood.waterball.teampathy.Controllers.MemberSystem.Exceptions.AccountDuplicatedException;
+import com.ood.waterball.teampathy.Controllers.MemberSystem.Exceptions.AccountNotFoundException;
+import com.ood.waterball.teampathy.Controllers.MemberSystem.Exceptions.ModelValidationException;
 import com.ood.waterball.teampathy.Domains.Member;
 
 public abstract class MemberController {
-    public abstract void logIn(String account,String password);
-    public abstract void register(Member member);
+    protected Member activeMember;
+
+    public abstract void logIn(String account,String password) throws AccountNotFoundException;
+    public abstract void register(Member member ,String passwordConfirm) throws ModelValidationException , AccountDuplicatedException;
 }
