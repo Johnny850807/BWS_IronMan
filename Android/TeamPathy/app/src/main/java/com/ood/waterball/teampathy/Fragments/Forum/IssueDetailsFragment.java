2ca4ab7e7e4df8b764b74e6a844ca95e6b08cc35
WaterBall <johnny850807@gmail.com>
2017-05-05 20:05:04 +0800

    issue details page layout

---
+++ Android/TeamPathy/app/src/main/java/com/ood/waterball/teampathy/Fragments/Forum/IssueDetailsFragment.java
@@ -0,0 +1,32 @@
+package com.ood.waterball.teampathy.Fragments.Forum;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.ood.waterball.teampathy.Fragments.ActivityBaseFragment;
+import com.ood.waterball.teampathy.R;
+
+
+public class IssueDetailsFragment extends ActivityBaseFragment {
+
+    @Override
+    protected void onFetchData(@Nullable Bundle savedInstanceState, @Nullable Bundle arguBundle) {
+
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.fragment_issue_details_page;
+    }
+
+    @Override
+    protected void onFindViews(View parentView) {
+
+    }
+
+    @Override
+    protected void onControlViews() {
+
+    }
+}
