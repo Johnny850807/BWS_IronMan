359e737a3c853e177e4340c5f914acb05acf1b60
WaterBall <johnny850807@gmail.com>
2017-07-05 21:32:18 +0800

    setHasMenu(true)

--- Android/TeamPathy/app/src/main/java/com/ood/waterball/teampathy/Fragments/TabLayoutPageFragment.java
+++ Android/TeamPathy/app/src/main/java/com/ood/waterball/teampathy/Fragments/TabLayoutPageFragment.java
@@ -7,10 +7,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.ood.waterball.teampathy.Controllers.Global;
 import com.ood.waterball.teampathy.DomainModels.Domains.Project;
@@ -56,6 +58,12 @@ public class TabLayoutPageFragment extends TemplateFragment {
     @Override
     protected int getLayoutResource() {
         return R.layout.fragment_tablayout_page;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
