98280880372fdabab5355726d6a7fa4f630d8e60
Johnny <johnny850807@gmail.com>
2017-07-07 14:22:36 +0800

    first contruct xml to node

--- Android/TeamPathy/app/src/main/java/com/ood/waterball/teampathy/DomainModels/WBS/XmlTranslatorImp.java
+++ Android/TeamPathy/app/src/main/java/com/ood/waterball/teampathy/DomainModels/WBS/XmlTranslatorImp.java
@@ -1,13 +1,85 @@
 package com.ood.waterball.teampathy.DomainModels.WBS;
 
+import android.content.res.AssetManager;
+import android.util.Xml;
+
+import com.ood.waterball.teampathy.Controllers.Global;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 public class XmlTranslatorImp extends XmlTranslator {
-	//todo xml / string conversion algorithm
-	public String taskToXml(TaskRoot taskRoot) {
+    private final String TASK_GROUP_NAME = "TaskGroup";
+    private final String TASK_NAME = "Task";
+    private final String NAME_ATT = "name";
+    private final String STARTDATE_ATT = "StartDate";
+    private final String DESCRIPTION_ATT = "Description";
+    private final String ID_ATT = "id";
+    private final String ENTDATE_ATT = "EndDate";
+
+    private TaskItem root;
+
+	//todo add id attributes
+	public String taskToXml(TaskItem taskRoot) {
 		return null;
 	}
 
-	public TaskRoot xmlToTasks() {
-		return null;
+	public TaskItem xmlToTasks(String xml) throws ParserConfigurationException, IOException, SAXException {
+        boolean hasRoot = false;
+        Document document = parseRootElement(xml);
+        Element rootElement = document.getDocumentElement();
+        TaskItem taskRoot = new TaskRoot(rootElement.getAttribute(NAME_ATT));
+		NodeList nodeList = rootElement.getChildNodes();
+        for ( int i = 0 ; i < nodeList.getLength() ; i ++ )
+        {
+            Node node = nodeList.item(i);
+            if ( node.getNodeType() == Node.ELEMENT_NODE )
+                taskRoot.addTaskChild(createTaskItem((Element) node));
+        }
+        return taskRoot;
 	}
+
+	private TaskItem createTaskItem(Element element){
+        TaskItem taskItem;
+        String name = element.getAttribute(NAME_ATT);
+        String ofGroup = ((Element)element.getParentNode()).getAttribute(NAME_ATT);
+        if (element.getTagName().equals(TASK_GROUP_NAME))
+        {
+            taskItem = new TaskGroup(ofGroup,name);
+            NodeList nodeList = element.getChildNodes();
+            for ( int i = 0 ; i < nodeList.getLength() ; i ++ )
+            {
+                Node node = nodeList.item(i);
+                if ( node.getNodeType() == Node.ELEMENT_NODE )
+                    taskItem.addTaskChild(createTaskItem((Element) node));
+            }
+        }
+        else
+        {
+            String description = element.getAttribute(DESCRIPTION_ATT);
+            taskItem = new TodoTask(ofGroup,name,description);
+        }
+
+        return taskItem;
+    }
+
+	private Document parseRootElement(String xml) throws IOException, SAXException, ParserConfigurationException {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(xml));
+        is.setEncoding("UTF-8");
+        return builder.parse(is);
+    }
 
 }
